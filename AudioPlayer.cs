c20c34766ac5237e3c60b40d271ce99fd08fa03e
ikeima <ikeima@yandex.ru>
2020-06-02 01:00:05 +0500

    Deploy!

---
+++ AudioPlayer.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Windows.Media;
+
+namespace Player
+{
+
+    public class AudioPlayer
+    {
+        public static MediaPlayer player = new MediaPlayer();
+
+        private const int MAX = 5; //максимальная длина массива
+
+        public static string[] library = new string[MAX]; //массив с именами аудиозаписей
+        public static string filename = null; //переменная для записи имени одного аудио-файла
+
+        public static void AudioLoad()
+        {
+            for (int i = 0; i < library.Length; i++) //поочередное заполнение массива
+            {
+                filename = Add_To_Arr(filename);
+                library[i] = filename;
+            }
+        }
+
+        public static string NextSong() //метод переключения песни на следующую
+        {
+            Random rand = new Random();
+            string audio = library[rand.Next(0, library.Length)]; //вытягивает случайную песню из массива
+
+            return audio; //возвращает ее имя для плеера
+        }
+
+        public static string Add_To_Arr(string filename) //метод для выбора аудио-файла
+        {
+            OpenFileDialog file = new OpenFileDialog();
+            file.Filter = "Audio(MP3)|*.MP3;|All files (ALL)|*.*";
+            file.ShowDialog();
+            filename = file.FileName;
+
+            return filename;
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Threading;
+
+namespace Player
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        MediaPlayer player = new MediaPlayer();
+        DispatcherTimer timer = new DispatcherTimer();
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            AudioPlayer.AudioLoad();
+
+            timer.Interval = TimeSpan.FromSeconds(0.1); //установка интервала тикания таймера в 1 секунду
+            timer.Tick += timer_tick;
+            player.MediaOpened += player_MediaOpened;
+        }
+
+        private void player_MediaOpened(object sender, EventArgs e) //событие при загрузке песни
+        {
+            timeMaxBlock.Text = player.NaturalDuration.TimeSpan.ToString(@"mm\:ss");
+            slider.Maximum = player.NaturalDuration.TimeSpan.TotalSeconds;
+        }
+
+        private void timer_tick(object sender, EventArgs e)
+        {
+            timeBlock.Text = player.Position.ToString(@"mm\:ss");
+            slider.Value = player.Position.TotalSeconds;
+        }
+
+        private void Pause_btn(object sender, RoutedEventArgs e)
+        {
+            player.Pause();
+            timer.Stop();
+        }
+
+        private void Resume_btn(object sender, RoutedEventArgs e)
+        {
+            player.Play();
+            timer.Start();
+        }
+
+        private void Next_btn(object sender, RoutedEventArgs e)
+        {
+            player.Open(new Uri(AudioPlayer.NextSong(), UriKind.RelativeOrAbsolute));
+            player.Play();
+        }
+
+        private void Start_btn(object sender, RoutedEventArgs e)
+        {
+            player.Open(new Uri(AudioPlayer.library[0], UriKind.RelativeOrAbsolute)); //загрузка в "плеер" аудио-файла и ее проигрывание
+            player.Play();
+            timer.Start();
+        }
+
+        private void slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            player.Position = TimeSpan.FromSeconds(slider.Value);
+        }
+
+
+    }
+}
